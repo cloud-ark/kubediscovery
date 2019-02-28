332844d0282cc264d8abb66e6d3b6275db9c5fa1
danielpygo <ddmoorish@gmail.com>
2019-02-28 12:07:09 -0600

    Reverted import back to "github.com/coreos/etcd/client"
    - Fixed issue 43

--- pkg/discovery/operatordocs.go
+++ pkg/discovery/operatordocs.go
@@ -9,7 +9,7 @@ import (
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
-	"go.etcd.io/etcd/client"
+	"github.com/coreos/etcd/client"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 )
