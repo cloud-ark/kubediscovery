bd19596a5faf5be63c535e1f0ed0885eb366c4fe
Devdatta Kulkarni <devdattakulkarni@gmail.com>
2020-06-07 05:55:17 -0500

    Clean up - Removed unused functions

--- pkg/discovery/utils.go
+++ pkg/discovery/utils.go
@@ -58,6 +58,8 @@ func getDynamicClient() (dynamic.Interface, error) {
 	return dynamicClient, err
 }
 
+// Composition utility functions
+
 func getComposition1(kind, name, status string, compositionTree *[]CompositionTreeNode) Composition {
 	var compositionString string
 	fmt.Printf("Kind: %s Name: %s Composition:\n", kind, name)
@@ -182,6 +184,8 @@ func printMaps() {
 	}
 }
 
+// Connection utility functions
+
 func PrintRelatives(format string, connections []Connection) {
 	switch format {
 	case "flat": 
@@ -197,21 +201,6 @@ func PrintRelatives(format string, connections []Connection) {
 
 func printConnectionsJSON(connections []Connection) {
 	fmt.Printf("%v", connections)
-}
-
-func adjustLevels(connections []Connection) []Connection {
-	for i, conni := range connections {
-		for _, connj := range connections {
-			// Update the level if parent-child relationship exists.
-			if conni.Name == connj.OwnerName && 
-			   conni.Kind == connj.OwnerKind && 
-			   conni.Namespace == connj.Namespace {
-			   conni.Level = connj.Level + 1
-			   connections[i] = conni
-			}
-		}
-	}
-	return connections
 }
 
 func printConnections(connections []Connection, printtype string) {
@@ -321,64 +310,6 @@ func compareConnectionsRelType(c1, c2 Connection) bool {
 	}
 }
 
-
-func comparePeers(c1, c2 Connection) bool {
-	if c1.Peer != nil && c2.Peer != nil {
-		if c1.Peer.Name != c2.Peer.Name {
-			return false
-		} else if c1.Peer.Kind != c2.Peer.Kind {
-			return false
-		} else {
-			return true
-		}
-	} else {
-		return true		
-	}
-}
-
-func appendRelNames(relativesNames []string, instanceName string) []string {
-	present := false
-	for _, relName := range relativesNames {
-		if relName == instanceName {
-			present = true
-			break
-		}
-	}
-	if !present {
-		relativesNames = append(relativesNames, instanceName)
-	}
-	return relativesNames
-}
-
-func appendRelatives(allRelatives, relatives []string) []string {
-	for _, rel := range relatives {
-		present := false 
-		for _, existingRel := range allRelatives {
-			if rel == existingRel {
-				present = true
-			}
-		}
-		if !present {
-			allRelatives = append(allRelatives, rel)			
-		}
-	}
-	return allRelatives
-}
-
-func checkPeerPresent(allConnections []Connection, peer, conn Connection) bool {
-	for _, c := range allConnections {
-		if c.Name == peer.Name && c.Kind == peer.Kind {
-			if c.Peer.Name == conn.Name && c.Peer.Kind == conn.Kind {
-				fmt.Printf("c:%v\n", c)
-				fmt.Printf("peer:%v\n", peer)
-				fmt.Printf("conn:%v\n", conn)
-				return true
-			}
-		}
-	}
-	return false
-}
-
 func AppendConnections(allConnections []Connection, connection Connection) []Connection {
 	present := false
 	present2 := false
@@ -422,86 +353,6 @@ func AppendConnections(allConnections []Connection, connection Connection) []Con
 	return allConnections
 }
 
-func AppendEdge(allConnections []Connection, connection Connection) []Connection {
-	kind := connection.Peer.Kind
-	name := connection.Peer.Name
-	namespace := connection.Peer.Namespace
-	present := false
-	for _, conn := range allConnections {
-		if conn.Name == name && conn.Kind == kind && conn.Namespace == namespace {
-			if conn.Peer.Name == connection.Name && conn.Peer.Kind == connection.Kind && conn.Peer.Namespace == connection.Namespace {
-				if conn.RelationType == connection.RelationType {
-					present = true
-					break
-				}
-			}
-		}
-	}
-	if !present {
-		allConnections = append(allConnections, connection)
-	}
-	return allConnections
-}
-
-func AppendEdge1(allConnections []Connection, connection Connection, peer Connection) []Connection {
-/*	present1 := false
-	present2 := false
-	store := false
-	for _, conn := range allConnections {
-		present1 = compareEdge(conn, connection)
-		present1 = compareConnection(conn, *connection.Peer)
-		if present1 || present2 {
-			break
-		}
-	}
-	if !present || !present2 {
-		allConnections = append(allConnections, connection)
-	}*/
-	present1 := false
-	present2 := false
-	for _, conn := range allConnections {
-		present1 = compareEdge(conn, connection, peer)
-		if present1 {
-			break
-		}
-	}
-	for _, conn := range allConnections {
-		present2 = compareConnections(conn, connection)
-		if present2 {
-			break
-		}
-	}
-	if !present1 {
-		allConnections = AppendConnections(allConnections, connection)
-	} else if !present2 {
-		allConnections = AppendConnections(allConnections, connection)
-	}
-	return allConnections
-}
-
-func compareEdge(c1, c2, c3 Connection) bool {
-	if c1.Kind == c2.Kind && c1.Name == c2.Name && c1.Namespace == c2.Namespace {
-		if c1.Peer.Kind == c3.Kind && c1.Peer.Name == c3.Name && c1.Peer.Namespace == c3.Namespace {
-			if c1.RelationType == c3.RelationType {
-				return true
-			}
-		}
-	}
-	return false
-/*	if c1.Kind != c2.Kind {
-		return false
-	} else if c1.Name != c2.Name {
-		return false
-	} else if c1.Namespace != c2.Namespace {
-		return false
-	} else if c1.RelationType != c2.RelationType {
-		return false 
-	} else {
-		return true
-	}
-*/
-}
-
 func appendConnections1(allConnections, connections []Connection) []Connection {
 	for _, conn := range connections {
 		present := false
@@ -525,99 +376,3 @@ func appendConnections1(allConnections, connections []Connection) []Connection {
 	}
 	return allConnections
 }
-
-func collectRelatives(relLeft, relRight []string) []string {
-	relatives := make([]string, 0)
-	for _, rel := range relLeft {
-		relatives = append(relatives, rel)
-	}
-	for _, rel := range relRight {
-		relatives = append(relatives, rel)
-	}
-	return relatives
-}
-
-func filterConnections1(connections []Connection, relativeNames []string) []string {
-	relativesToSearch := make([]string,0)
-	for _, relativeName := range relativeNames {
-		found := false
-		for _, currentRelative := range connections {
-			if currentRelative.Name == relativeName {
-				found = true
-			}
-		}
-		if !found {
-			relativesToSearch = append(relativesToSearch, relativeName)
-		}
-	}
-	return relativesToSearch
-}
-
-
-func filterRelatives(connections []Connection, relativeNames []string) []string {
-	relativesToSearch := make([]string,0)
-	for _, relativeName := range relativeNames {
-		found := false
-		for _, currentRelative := range connections {
-			if currentRelative.Name == relativeName {
-				found = true
-			}
-		}
-		if !found {
-			relativesToSearch = append(relativesToSearch, relativeName)
-		}
-	}
-	return relativesToSearch
-}
-
-func checkHistory(connections []Connection, kind, instance, namespace string) bool {
-	present := false
-	for _, conn := range connections {
-		if conn.Name == instance &&
-		   conn.Kind == kind &&
-		   conn.Namespace == namespace {
-		   	present = true
-		}
-	}
-	return present
-}
-
-func searchOwnerGraph(connections, owners []Connection, level int) []Connection {
-	level = level + 1
-	unseenOwners := make([]Connection,0)
-	for _, owner := range owners {
-		present := false
-		for _, connection := range connections {
-			if connection.Name == owner.Name && connection.Kind == owner.Kind && connection.Namespace == owner.Namespace {
-				present = true
-			}
-		}
-		if !present {
-			unseenOwners = append(unseenOwners, owner)
-		}
-	}
-	for _, relative := range unseenOwners {
-		kind := relative.Kind
-		name := relative.Name
-		namespace := relative.Namespace
-		connections = GetRelatives(connections, level, kind, name, "", "", namespace, "")
-	}
-	return connections
-} 
-
-func trackEdges(nodes []Connection, level int, relType, kind, instance, namespace string) {
-	fmt.Printf("Tot1:%v\n", TotalClusterConnections)
-	for _, node := range nodes {
-		newNode := deepCopy(node)
-		newNode.Peer = &Connection{
-			Name: instance,
-			Kind: kind,
-			Namespace: namespace,
-		}
-		newNode.Level = level
-		newNode.RelationType = relType
-		TotalClusterConnections = AppendEdge(TotalClusterConnections, newNode)
-	}
-	fmt.Printf("Tot2:%v\n", TotalClusterConnections)
-}
-
